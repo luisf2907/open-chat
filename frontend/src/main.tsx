cdedcc98a57eabd6d4946d4c2c07dcf718abd7a8
mathfemar <mathfemar@gmail.com>
2025-08-19 10:44:43 -0300

    internacionalizacao

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './i18n' // Inicializa i18next
 import App from './App.tsx'
 import { ThemeProvider } from './contexts/ThemeContext'
 
